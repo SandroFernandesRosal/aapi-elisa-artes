ebfb3cd4a6ea822493d008b75a6683cb0394d2ec
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2024-08-16 19:51:48 -0300

    .

---
+++ src/app/api/savethedate/featured/route.ts
@@ -0,0 +1,11 @@
+import data from '../data.json'
+
+export async function GET() {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
+  const featuredProducts = data.savethedate.filter(
+    (product) => product.featured,
+  )
+
+  return Response.json(featuredProducts)
+}
